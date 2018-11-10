df57532791dba5af67696bf27d2a00a9cd591711
Luis Sanchez <sanchezl391@gmail.com>
2018-11-09 22:02:35 -0800

    localDemo now runs client code and an example of using the server method

--- graph/localDemo.py
+++ graph/localDemo.py
@@ -1,16 +1,44 @@
-from node import *
+# minimalistic client example from 
+# https://github.com/seprich/py-bson-rpc/blob/master/README.md#quickstart
 
+import socket
+from bsonrpc import JSONRpc
+from bsonrpc.exceptions import FramingError
+from bsonrpc.framing import (
+	JSONFramingNetstring, JSONFramingNone, JSONFramingRFC7464)
+from node import *
+import json
+
+# Cut-the-corners TCP Client:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('localhost', 50001))
+
+rpc = JSONRpc(s,framing_cls=JSONFramingNone)
+server = rpc.get_peer_proxy()
+
+def recursiveUpdateRoot(rootStrObj, root):
+	# update node value
+	root.val = rootStrObj["val"]
+	# iterate through children to update value
+	for index, child in enumerate(root.children):
+		recursiveUpdateRoot( rootStrObj["children"][index], child )
+
+# Client Node demo
 leaf1 = node("leaf1")
 leaf2 = node("leaf2")
 
-root = node("root", [leaf1, leaf1, leaf2])
+root = node("root", [leaf1, leaf2])
 
-print("graph before increment")
+print("graph before increment:")
+root.show()
+print("\n")
+
+rootStr = str(root) # make dict str out of dict/ class obj
+rootStr = server.increment(rootStr)
+
+rootStrObj = eval(rootStr) # evaluate str as python dict
+recursiveUpdateRoot(rootStrObj, root) # update root w/ new values
+print("graph after increment:")
 root.show()
 
-# do this increment remotely:
-increment(root)
-
-print("graph after increment")
-root.show()
-
+rpc.close() # Closes the socket 's' also
